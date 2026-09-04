2adb8c040814e15ef440db65009e45995988c702
thomasblondet <blondet.thomas@protonmail.com>
2026-09-04 15:36:55 +0200

    add hostname resolution

--- main.c
+++ main.c
@@ -1,5 +1,42 @@
 #include "traceroute.h"
 
-int main(void) {
+static void fatal(const char *str) {
+    fprintf(stderr, "traceroute: ");
+    perror(str);
+    exit(1);
+}
+
+static void hostname_resolution(Host *h) {
+    const struct addrinfo hints = {
+        .ai_flags = 0,
+        .ai_family = AF_INET,
+        .ai_socktype = 0,
+        .ai_protocol = 0,
+        .ai_addrlen = 0,
+        .ai_addr = NULL,
+    };
+
+    struct addrinfo *res = NULL;
+    const int ret = getaddrinfo(h->hostname, NULL, &hints, &res);
+    if (ret != 0) {
+        printf("ft_ping: cannot resolve %s: %s\n", h->hostname, gai_strerror(ret));
+        exit(1);
+    }
+
+    const struct sockaddr_in *sin = (const struct sockaddr_in *)res->ai_addr;
+    inet_ntop(AF_INET, &sin->sin_addr, h->ip, INET_ADDRSTRLEN);
+    h->addr.sin_family = AF_INET;
+    memcpy(&h->addr.sin_addr, &sin->sin_addr, sizeof(sin->sin_addr));
+
+    freeaddrinfo(res);
+}
+
+int main(int argc, char *argv[]) {
+    Host host = {0};
+
+    (void)argc;
+    memcpy(host.hostname, argv[1], strlen(argv[1]));
+    hostname_resolution(&host);
+    printf("%s %s\n", host.hostname, host.ip);
     return 0;
 }
--- traceroute.h
+++ traceroute.h
@@ -1,4 +1,19 @@
 #ifndef TRACEROUTE_H
 #define TRACEROUTE_H
 
+#include <stdio.h>
+#include <netdb.h>
+#include <string.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+typedef struct {
+    int fd;
+    char hostname[NI_MAXHOST];
+    char ip[INET_ADDRSTRLEN];
+    struct sockaddr_in addr;
+    unsigned int ttl;
+} Host;
+
 #endif
